d4df457a79f89ba6ddb9f65cfc65e03a221f6fe3
ukilaru <89611164+ukilaru@users.noreply.github.com>
2021-08-26 18:42:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 
-from flask import Flask,jsonify,request
+from flask import Flask,request,jsonify
+from flask_cors import CORS
 import recommendation
-# from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
-
-# CORS(app)
-
+CORS(app) 
+        
 @app.route('/movie', methods=['GET'])
 def recommend_movies():
-    res = recommendation.results(request.args.get('title'))
-    return jsonify(res)
+        res = recommendation.results(request.args.get('title'))
+        return jsonify(res)
 
 if __name__=='__main__':
-    app.run(port = 5000, debug = True)
+        app.run(port = 5000, debug = True)
 
